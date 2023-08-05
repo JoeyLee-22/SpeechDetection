e7ef931d74820449f17b35d28d3ceb665eb0c102
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2023-08-05 21:21:40 +0900

    2.0.0
    update config to work with mel frequency model

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import json
 
-config = {"demo_mode":False,
-          "is_logging":True,
-          "log_folder":"logV2",
-          "is_graphing":False,
-          "sentence_start_sec":10, 
-          "sentence_end_sec":14, 
-          "sweep_start":0, 
-          "sweep_end":10}
+# config = {"demo_mode":False,
+#           "is_logging":True,
+#           "log_folder":"logV8",
+#           "is_graphing":False,
+#           "sentence_start_sec":9, 
+#           "sentence_end_sec":13, 
+#           "sweep_start":0, 
+#           "sweep_end":10}
+
+config = {"model_filename":"speech_detection_model3.joblib",
+          "audio_filename":"logV7/07-16,17:08:26,a=6.40,SNR=0.02/s_prime.wav",
+          "train":False,
+          "load_and_predict":True}
 
 with open('config.json', 'w') as f:
     json.dump(config, f)
